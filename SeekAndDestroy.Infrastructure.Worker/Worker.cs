cbef862aff8f783f0e29f581063a2db91caf326e
nightblade9 <nightbladecodes@gmail.com>
2019-12-07 14:05:28 -0500

    Simplify `Worker` use of config so it's only loaded once

--- SeekAndDestroy.Infrastructure.Worker/Worker.cs
+++ SeekAndDestroy.Infrastructure.Worker/Worker.cs
@@ -14,22 +14,26 @@ namespace SeekAndDestroy.Infrastructure.Worker
     public class Worker : BackgroundService
     {
         private readonly ILogger<Worker> _logger;
+        private readonly IConfigurationRoot _configuration;
 
         public Worker(ILogger<Worker> logger)
         {
             _logger = logger;
+            _configuration = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build();
+
+            // Run on execute
+            this.ExecuteAsync(new CancellationToken());
         }
 
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
         {
-            var userRepository = new UserRepository(this.Config()["ConnectionString"]);
+            var userRepository = new UserRepository(_configuration["ConnectionString"]);
             var ticker = new Ticker(userRepository);
             while (!stoppingToken.IsCancellationRequested)
             {
                 ticker.DoTick();
-                await Task.Delay(int.Parse(this.Config()["MinutesPerIncrement"]) * 1000 * 60, stoppingToken);
+                await Task.Delay(int.Parse(_configuration["MinutesPerIncrement"]) * 1000 * 60, stoppingToken);
             }
         }
-        public IConfigurationRoot Config() => new ConfigurationBuilder().AddJsonFile("appsettings.json").Build();
     }
 }
